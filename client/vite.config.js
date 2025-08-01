0fd56fb93feb38534f4fe6b9fafa72ee473d281b
00JAINISH00 <jainishthakor5267@gmail.com>
2025-08-01 21:34:34 +0530

    Fix: added tailwindcss plugin

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from 'vite-plugin-tailwindcss'
 
 
 // https://vite.dev/config/
